3916e35c3c70cd5aac4f627c7bb6473268bf75ed
hsedaqat1378 <hoseinsedaqat6@gmail.com>
2022-01-09 09:54:51 +0330

    change correct validator for email

--- src/utils/Validations.js
+++ src/utils/Validations.js
@@ -1,3 +1,3 @@
-const validationEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-// const validationEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+// const validationEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const validationEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export { validationEmail }
