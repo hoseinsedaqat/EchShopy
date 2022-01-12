26ee69a4471b08deac86f968556af9ba9074952b
hsedaqat1378 <hoseinsedaqat6@gmail.com>
2022-01-12 10:21:07 +0330

    regx final test and refactor

--- src/utils/Validations.js
+++ src/utils/Validations.js
@@ -1,3 +1,3 @@
-// const validationEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+// define my Regex Validation in here:
 const validationEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export { validationEmail }
