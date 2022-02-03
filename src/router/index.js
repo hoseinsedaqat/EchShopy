9db97f96491c1493c9fef9ce86339d9c08790785
hsedaqat1378 <hoseinsedaqat6@gmail.com>
2022-02-03 11:09:32 +0330

    done with router fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import ProfileDataForm from '../views/ProfileDataForm.vue'
 
 // Check for Authentication
 import { checkAuth } from '../utils/Auth'
@@ -15,7 +14,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')
+    component: async () => await import('../views/Home.vue')
   },
   {
     path: '/aboutus',
@@ -23,22 +22,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/About.vue')
+    component: async () => await import('../views/About.vue')
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/Login.vue')
+    component: async () => await import('../views/Login.vue')
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: () => import('../views/Signup.vue')
+    component: async () => await import('../views/Signup.vue')
   },
   {
     path: '/cart',
     name: 'Cart',
-    component: () => import('../views/Cart.vue')
+    component: async () => await import('../views/Cart.vue')
   },
   {
     path: '/contact',
@@ -53,61 +52,61 @@ const routes = [
   {
     path: '/product',
     name: 'ProdcutPage',
-    component: () => import('../views/ProductPage.vue')
+    component: async () => await import('../views/ProductPage.vue')
   },
   {
     path: '/books',
     name: 'BookPage',
-    component: () => import('../views/BookPage.vue')
+    component: async () => await import('../views/BookPage.vue')
   },
   // ErrorPage
   {
     path: '*',
     name: 'ErrorPage',
-    component: () => import('../views/Error.vue')
+    component: async () => await import('../views/Error.vue')
   },
   {
     path: '/book/:id',
     name: 'ProductBook',
-    component: () => import('../views/ProductBook.vue')
+    component: async () => await import('../views/ProductBook.vue')
   },
   {
     path: '/laptop/:id',
     name: 'ProductLaptop',
-    component: () => import('../views/ProductLaptop.vue')
+    component: async () => await import('../views/ProductLaptop.vue')
   },
   {
     path: '/mobile/:id',
     name: 'ProductMobile',
-    component: () => import('../views/ProductMobile.vue')
+    component: async () => await import('../views/ProductMobile.vue')
   },
   {
     path: '/forgotpassword',
     name: 'ForgotPassword',
-    component: () => import('../views/ForgotPassword.vue')
+    component: async () => await import('../views/ForgotPassword.vue')
   },
   {
     path: '/books/:id',
     name: 'EachBookPage',
-    component: () => import('../views/BookPage.vue')
+    component: async () => await import('../views/BookPage.vue')
   },
   {
     path: '/profile',
     name: 'Profile',
     beforeEnter: checkAuth,
-    component: () => import('../views/Profile.vue')
+    component: async () => await import('../views/Profile.vue')
   },
   {
     path: '/profile/favoritelist',
     name: 'FavoriteList',
     beforeEnter: checkAuth,
-    component: () => import('../views/ProfileFavoriteList.vue')
+    component: async () => await import('../views/ProfileFavoriteList.vue')
   },
   {
     path: '/profile/update-profile',
     name: 'ProfileDataForm',
     beforeEnter: checkAuth,
-    component: ProfileDataForm
+    component: async () => await import('../views/ProfileDataForm.vue')
   }
 ]
 
