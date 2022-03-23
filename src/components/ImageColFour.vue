0f41099169068055a30ab29455511b37f2d05dfe
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-23 21:05:17 +0430

    refactor image col fpur

--- src/components/ImageColFour.vue
+++ src/components/ImageColFour.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="image-col-four">
-    <div>
+    <div v-for="(ic,idx) in ImageColFour" :key="(ic,idx)">
       <img
-        src="../assets/images/Ads/2012f96808c281cf5737c5ff405b9b89d364e7fe_1634041614.jpg"
-        alt="Image2"
-      />
-    </div>
-    <div>
-      <img
-        src="../assets/images/Ads/904db8a169ce8fdc6493e21b6948d3e32a19ae68_1626258276.jpg"
-        alt="Image1"
-      />
-    </div>
-    <div>
-      <img
-        src="../assets/images/Ads/4733b740d15e74f00d50ac92fb126911632b8053_1599385682.jpg"
-        alt="Image4"
-      />
-    </div>
-    <div>
-      <img
-        src="../assets/images/Ads/956cd52f1f18f11284016c86561d53bcdcfdeedd_1612606849.jpg"
-        alt="Image3"
+        :src="ic.img"
+        :alt="ic.id"
       />
     </div>
   </div>
 </template>
 
 <script>
+import ImageColFour from "../data/ImageColFour/imagecolfour";
 export default {
   name: "ImageColFour",
+  data() {
+    return {
+      ImageColFour,
+    };
+  },
 };
 </script>
---
+++ src/data/ImageColFour/imagecolfour.js
@@ -0,0 +1,24 @@
+import ImageColOne from '../../assets/images/Ads/2012f96808c281cf5737c5ff405b9b89d364e7fe_1634041614.jpg'
+import ImageColTwo from '../../assets/images/Ads/904db8a169ce8fdc6493e21b6948d3e32a19ae68_1626258276.jpg'
+import ImageColThree from '../../assets/images/Ads/4733b740d15e74f00d50ac92fb126911632b8053_1599385682.jpg'
+import ImageColFour from '../../assets/images/Ads/956cd52f1f18f11284016c86561d53bcdcfdeedd_1612606849.jpg'
+
+
+export default [
+    {
+        id: 1,
+        img: ImageColOne
+    },
+    {
+        id: 2,
+        img: ImageColTwo
+    },
+    {
+        id: 3,
+        img: ImageColThree
+    },
+    {
+        id: 4,
+        img: ImageColFour
+    }
+]
